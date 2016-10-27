c0b21c39b06cd50e093d667c002080d4bcd5e7cb
Dan Benton <dan@dogsbody.org>
2016-10-27 19:30:53 +0100

    cleanup

--- install.sh
+++ install.sh
@@ -3,41 +3,51 @@
 # Description: Installation script for displayboard
 #
 
-sudo apt-get install -y matchbox epiphany-browser xwit xinit
+# Check we are root
+if [ $LOGNAME != "root" ]
+then
+    echo "ERROR: Execution of $0 stopped as not run by user root!"
+    exit 1
+fi
+
+# Install the bits we need
+apt install -y matchbox epiphany-browser xwit xinit ttf-mscorefonts-installer
+
+# Setup rc.local
+cp /home/pi/raspbian-jessie-epiphany-display/rc.local /etc/rc.local
+
+# Setup .xinitrc
+sudo -u pi cp /home/pi/raspbian-jessie-epiphany-display/xinitrc /home/pi/.xinitrc
+
+# Allow anyone to start an Xserver
+echo x11-common x11-common/xwrapper/allowed_users select Anybody | debconf-set-selections -v
+echo x11-common x11-common/xwrapper/actual_allowed_users string anybody | debconf-set-selections -v
+
+# Download and copile the latest cec-client so we can turn the TV on and off via cron
+# echo "on 0" | /usr/local/bin/cec-client -s
+# echo "standby 0" | /usr/local/bin/cec-client -s
+apt install -y cmake libudev-dev libxrandr-dev python-dev swig
+
+sudo -u pi git clone https://github.com/Pulse-Eight/platform.git /home/pi/cec-platform
+cd /home/pi/cec-platform
+sudo -u pi cmake .
+sudo -u pi make
+make install
+
+sudo -u pi git clone https://github.com/Pulse-Eight/libcec.git /home/pi/libcec
+cd /home/pi/libcec
+sudo -u pi cmake -DRPI_INCLUDE_DIR=/opt/vc/include -DRPI_LIB_DIR=/opt/vc/lib .
+sudo -u pi make -j4
+make install
+ldconfig
 
 
+#sudo apt-get install  x11-xserver-utils xautomation unclutter
+#sudo apt-get install -y sqlite3 vim at
+#sudo update-alternatives --config editor
+#sudo ln -s /usr/lib/arm-linux-gnueabihf/nss/ /usr/lib/nss
 
 
-sudo apt-get install epiphany-browser x11-xserver-utils xautomation unclutter
-
-
-
-sudo apt-get update
-sudo apt-get -y dist-upgrade
-sudo apt-get install -y matchbox chromium ttf-mscorefonts-installer xwit sqlite3 vim at
-sudo update-alternatives --config editor
-sudo ln -s /usr/lib/arm-linux-gnueabihf/nss/ /usr/lib/nss
-sudo vim /etc/rc.local
-  # See rc.local file in this folder
-sudo vim .xinitrc
-  # See xinitrc file in this folder
-crontab -e 
-  00 08 * * 1-5 echo "on 0" | /usr/local/bin/cec-client -s > /dev/null
-  00 18 * * 1-5 echo "standby 0" | /usr/local/bin/cec-client -s > /dev/null
-sudo reboot
-
-**__CEC INstall__**
-# For some reason the libcec1 cec-utils packages are broken on Raspbian
-git clone https://github.com/Pulse-Eight/libcec 
-sudo apt-get install gcc liblockdev1-dev autoconf automake libtool checkinstall libudev-dev cmake
-cd libcec
-./bootstrap
-./configure --with-rpi-include-path="/opt/vc/include" --with-rpi-lib-path="/opt/vc/lib/" --enable-rpi
-make
-sudo checkinstall
-sudo ldconfig
-/usr/local/bin/cec-client
-echo "on 0" | /usr/local/bin/cec-client -s
-echo "standby 0" | /usr/local/bin/cec-client -s
-
-
+#crontab -e 
+#  00 08 * * 1-5 echo "on 0" | /usr/local/bin/cec-client -s > /dev/null
+#  00 18 * * 1-5 echo "standby 0" | /usr/local/bin/cec-client -s > /dev/null
